d5b8e7e695651d648072322fea4758bc55c45535
Bruno-F2 <67654075+Bruno-F2@users.noreply.github.com>
2026-09-14 19:12:57 -0300

    Primer Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()],
+
+  server: {
+    port: 3000,
+    open: true,
+    allowedHosts: true,
+    host: true,
+  }
+});
